f9d553df6ec24ea838faac9d7c222e227be66c75
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-02-24 13:52:59 +0700

    remake publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 PUBLISH_DIRECTORY="published"
 
+if [ ! "$1" ]; then
+  echo "Usage: $0 <directory> <solution (true for include the solution)>"
+  exit 1
+fi
+
 echo "Publishing $1 to Standalone Questions Test"
 if [ ! -d "$PUBLISH_DIRECTORY" ]; then
     echo "Creating $PUBLISH_DIRECTORY directory"
@@ -13,23 +18,36 @@ if [ -d "$PUBLISH_DIRECTORY/$1" ]; then
 fi
 
 echo "Creating $PUBLISH_DIRECTORY/$1 structure"
-cp -r _template/publish-template $PUBLISH_DIRECTORY
 if [ ! -d $PUBLISH_DIRECTORY/$(dirname "$1") ]; then
   mkdir $PUBLISH_DIRECTORY/$(dirname "$1")
 fi
-mv $PUBLISH_DIRECTORY/publish-template $PUBLISH_DIRECTORY/$1
+
+mkdir -p $PUBLISH_DIRECTORY/$1
 
 echo "Creating Test setup for $1"
 find $PUBLISH_DIRECTORY/$1/. -maxdepth 1 -type f -delete
-rm -rf $PUBLISH_DIRECTORY/$1/run/__tests__
-cp -r $1/run/__tests__ $PUBLISH_DIRECTORY/$1/run/
+mkdir -p $PUBLISH_DIRECTORY/$1/run/__tests__ && cp -r $1/run/__tests__ $PUBLISH_DIRECTORY/$1/run
 cp -r $1/*.js $PUBLISH_DIRECTORY/$1/
 cp -r $1/*.md $PUBLISH_DIRECTORY/$1/
 sed -e "s/${1/\//"\/"}/../g" -i $PUBLISH_DIRECTORY/$1/run/__tests__/*.js
-cd $PUBLISH_DIRECTORY/$1
+cp -r package $PUBLISH_DIRECTORY/$1/run
+cp -r $1/run/index.js $PUBLISH_DIRECTORY/$1/run/index.js
+sed -e "s/..\/..\/..\//.\//g" -i $PUBLISH_DIRECTORY/$1/run/index.js
 
-echo "Bundling $1 to ${1/\//"-"}.zip"
-zip -r ../../${1/\//"-"}.zip ./* && cd ../../..
+if [ "$2" ]; then
+  echo "Adding solution for $1"
+  mkdir -p $PUBLISH_DIRECTORY/$1/solution
+  cp -r $1/solution/*.js $PUBLISH_DIRECTORY/$1/solution
+fi
+
+cd $PUBLISH_DIRECTORY/$1
+if [ "$2" ]; then
+  echo "Bundling $1 to ${1/\//"-"}-with-solution.zip"
+  zip -r ../../${1/\//"-"}-with-solution.zip ./* && cd ../../..
+else 
+  echo "Bundling $1 to ${1/\//"-"}.zip"
+  zip -r ../../${1/\//"-"}.zip ./* && cd ../../..
+fi
 
 echo "Cleaning up $PUBLISH_DIRECTORY/$1"
 rm -rf $PUBLISH_DIRECTORY/$(dirname "$1")
