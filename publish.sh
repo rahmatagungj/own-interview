3d4bce23aef70081f2a60a0fb61d0c10d5b1f9bf
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-02-24 13:04:06 +0700

    remapping

--- publish.sh
+++ publish.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
-FILE="published"
+PUBLISH_DIRECTORY="published"
 
-echo "Publishing $1 to Standalone Questions Testing"
-if [ ! -d "$FILE" ]; then
-    echo "Creating $FILE directory"
-    mkdir $FILE
+echo "Publishing $1 to Standalone Questions Test"
+if [ ! -d "$PUBLISH_DIRECTORY" ]; then
+    echo "Creating $PUBLISH_DIRECTORY directory"
+    mkdir $PUBLISH_DIRECTORY
 fi
 
-if [ -d "$FILE/$1" ]; then
-    echo "Removing $FILE/$1 directory (already exists)"
-    rm -rf $FILE/$1
+if [ -d "$PUBLISH_DIRECTORY/$1" ]; then
+    echo "Removing $PUBLISH_DIRECTORY/$1 directory (already exists)"
+    rm -rf $PUBLISH_DIRECTORY/$1
 fi
 
-echo "Creating $FILE/$1 structure"
-cp -r _template/publish-template $FILE
-if [ ! -d $FILE/$(dirname "$1") ]; then
-  mkdir $FILE/$(dirname "$1")
+echo "Creating $PUBLISH_DIRECTORY/$1 structure"
+cp -r _template/publish-template $PUBLISH_DIRECTORY
+if [ ! -d $PUBLISH_DIRECTORY/$(dirname "$1") ]; then
+  mkdir $PUBLISH_DIRECTORY/$(dirname "$1")
 fi
-mv $FILE/publish-template $FILE/$1
+mv $PUBLISH_DIRECTORY/publish-template $PUBLISH_DIRECTORY/$1
 
 echo "Creating Test setup for $1"
-find $FILE/$1/. -maxdepth 1 -type f -delete
-rm -rf $FILE/$1/run/__tests__
-cp -r $1/run/__tests__ $FILE/$1/run/
-cp -r $1/*.js $FILE/$1/
-cp -r $1/*.md $FILE/$1/
-sed -e "s/${1/\//"\/"}/../g" -i $FILE/$1/run/__tests__/*.js
-cd $FILE/$1
+find $PUBLISH_DIRECTORY/$1/. -maxdepth 1 -type f -delete
+rm -rf $PUBLISH_DIRECTORY/$1/run/__tests__
+cp -r $1/run/__tests__ $PUBLISH_DIRECTORY/$1/run/
+cp -r $1/*.js $PUBLISH_DIRECTORY/$1/
+cp -r $1/*.md $PUBLISH_DIRECTORY/$1/
+sed -e "s/${1/\//"\/"}/../g" -i $PUBLISH_DIRECTORY/$1/run/__tests__/*.js
+cd $PUBLISH_DIRECTORY/$1
 
 echo "Bundling $1 to ${1/\//"-"}.zip"
 zip -r ../../${1/\//"-"}.zip ./* && cd ../../..
 
-echo "Cleaning up $FILE/$1"
-rm -rf $FILE/$(dirname "$1")
+echo "Cleaning up $PUBLISH_DIRECTORY/$1"
+rm -rf $PUBLISH_DIRECTORY/$(dirname "$1")
 
-echo "$1 successfully published to Standalone Questions Testing"
+echo "$1 successfully published to Standalone Questions Test"
